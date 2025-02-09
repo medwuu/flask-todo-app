6e4fea931ffb9980ca41f0774a136ea241ecc6a3
medwuu <aepegon@gmail.com>
2025-02-09 19:43:24 +0300

    feat: конфигурация приложения по умолчанию

--- app/config.py
+++ app/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SESSION_TYPE = os.getenv("SESSION_TYPE")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SESSION_TYPE = os.getenv("SESSION_TYPE", "filesystem")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///todo.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "change me")
     SESSION_USE_SIGNER = True
-    # DEBUG
-    SQLALCHEMY_ECHO = True
+    DEBUG = os.getenv("DEBUG", False)
+    SQLALCHEMY_ECHO = os.getenv("SQLALCHEMY_ECHO", False)
